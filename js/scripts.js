99d87eb00af5797e913aa5e0da3b40a33a22c9e5
Anthony Gold <anthgold@gmail.com>
2016-04-01 11:51:40 -0700

    substitutes ping for 3

--- js/scripts.js
+++ js/scripts.js
@@ -4,20 +4,26 @@ $(document).ready(function() {
     event.preventDefault();
     // Take user input and count up from 1 to the input
     var numberRange = $("input#number").val();
-    // numberRange is a string
-    var integer = parseInt(numberRange);
-    // integer is a number
+      // numberRange is a string
+    // var integer = parseInt(numberRange);
+      // integer is a number
+    // If i is divisible by 3, substitute "ping" for string i
+      // i will need to be converted to a string in a new variable
+
 
 // User Interface Logic
     for (var i = 1; i <= numberRange; i++) {
       // i is also of type number
-      $("ul#result").append("<li>" + i + "</li>")
+      var iAsString = i.toString();
+      if (i % 3 === 0) {
+        iAsString = "ping";
+      }
+      $("ul#result").append("<li>" + iAsString + "</li>")
     }
-    console.log(typeof numberRange);
-    console.log(typeof integer);
-    console.log(typeof i);
 
     // $("#result").show();
+
+    // To-do: Hide old results on a re-submit
 
   });
 });
