75f7d42bb0dd5150db88bdf0cec1d33c8b2d7ebb
Anthony Gold <anthgold@gmail.com>
2016-04-01 10:09:04 -0700

    converts input from string to integer

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,17 @@ $(document).ready(function() {
   $("form#number-taker").submit(function(event) {
     event.preventDefault();
     // Take user input and count up from 1 to the input
-    var number =
-
+    var numberRange = $("input#number").val();
+    var integer = parseInt(numberRange);
 
 // User Interface Logic
-
-
+    for (var i = 1; i <= numberRange; i++) {
+      // alert(i);
+      // To-do output a string counting up to the integer
+    }
+    console.log(typeof numberRange);
+    console.log(typeof ("input#number"));
+    console.log(typeof integer);
 
     $("#result").show();
 
