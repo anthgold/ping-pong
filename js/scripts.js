7faa9d53bc17f91246d3f94f0300976dabecc30b
Anthony Gold <anthgold@gmail.com>
2016-04-01 14:51:19 -0700

    takes number input and returns either a number or a substitute; business and ui logic intermingled hopelessly

--- js/scripts.js
+++ js/scripts.js
@@ -2,18 +2,8 @@
 $(document).ready(function() {
   $("form#number-taker").submit(function(event) {
     event.preventDefault();
-    // Take user input and count up from 1 to the input
     var numberRange = $("input#number").val();
-      // numberRange is a string
-    // var integer = parseInt(numberRange);
-      // integer is a number
-    // If i is divisible by 3, substitute "ping" for string i
-      // i will need to be converted to a string in a new variable
-
-
-// User Interface Logic
     for (var i = 1; i <= numberRange; i++) {
-      // i is also of type number
       var iAsString = i.toString();
       if (i % 15 === 0) {
         iAsString = "pingpong";
@@ -22,12 +12,9 @@ $(document).ready(function() {
       } else if (i % 5 === 0) {
         iAsString = "pong";
       }
+// User Interface Logic
       $("ul#result").append("<li>" + iAsString + "</li>")
     }
-
-    // $("#result").show();
-
     // To-do: Hide old results on a re-submit
-
   });
 });
