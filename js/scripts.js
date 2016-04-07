d71cbbc4f45eb59bfa375fe685791f92216f96c5
Anthony Gold <anthgold@gmail.com>
2016-04-06 20:02:21 -0700

    separates business from ui logic; returns more than needed

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,33 @@
 // Business Logic
+var pingPongArray = [];
+
+var pingPong = function(userInput) {
+  for (var i = 1; i <= userInput; i++) {
+    if (i % 15 === 0) {
+      pingPongArray.push ("pingpong");
+    } else if (i % 3 === 0) {
+      pingPongArray.push ("ping");
+    } else if (i % 5 === 0) {
+      pingPongArray.push ("pong");
+    } else {
+      pingPongArray.push (i);
+    }
+  }
+}
+
+// User Interface Logic
 $(document).ready(function() {
   $("form#number-taker").submit(function(event) {
     event.preventDefault();
+
     var numberRange = $("input#number").val();
-    for (var i = 1; i <= numberRange; i++) {
-      var iAsString = i.toString();
-      if (i % 15 === 0) {
-        iAsString = "pingpong";
-      } else if (i % 3 === 0) {
-        iAsString = "ping";
-      } else if (i % 5 === 0) {
-        iAsString = "pong";
-      }
-// User Interface Logic
-      $("ul#result").append("<li>" + iAsString + "</li>")
-    }
-    // To-do: Hide old results on a re-submit
+    var result = pingPong(numberRange);
+
+    pingPongArray = pingPongArray.join(", ");
+
+    // As it counts up, append
+    for (var i = 0; i < numberRange; i++) {
+      $("ul#result").append("<li>" + pingPongArray + "</li>")
+    };
   });
 });
